a0ec7432e8ccc6af57421a49acf0eab454cd7883
Luke Mohr <luke.mohr@revature.net>
2021-04-20 10:01:55 -0600

    Authenticated user is no longer hardcoded, set default values for angler authentication booleans

--- src/main/java/com/fishinginstreams/model/Angler.java
+++ src/main/java/com/fishinginstreams/model/Angler.java
@@ -36,10 +36,10 @@ public class Angler implements UserDetails {
 
     // UserDetail fields
     private ArrayList<? extends GrantedAuthority> authorities;
-    private boolean accountNonExpired;
-    private boolean accountNonLocked;
-    private boolean credentialsNonExpired;
-    private boolean enabled;
+    private boolean accountNonExpired = true;
+    private boolean accountNonLocked = true;
+    private boolean credentialsNonExpired = true;
+    private boolean enabled = true;
 
     @OneToMany(mappedBy = "angler")
     private List<Catch> catches;
